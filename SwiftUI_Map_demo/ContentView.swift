2bc1803f7b8b490d0eb4480f01de7d5983ecd51f
Tom Montgomery <montgomery3681@gmail.com>
2023-09-20 22:56:43 -0600

    Initial Commit

---
+++ SwiftUI_Map_demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Map_demo
+//
+//  Created by tom montgomery on 9/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Map_demo/SwiftUI_Map_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Map_demoApp.swift
+//  SwiftUI_Map_demo
+//
+//  Created by tom montgomery on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Map_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
