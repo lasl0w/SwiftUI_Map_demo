c3bb0a6bf2d62569fa6157b4764754a6201cf821
Tom Montgomery <montgomery3681@gmail.com>
2023-09-27 17:57:53 -0600

    - add beach and park buttons - make search requests and place on map view - add state and map controls

---
+++ SwiftUI_Map_demo/BeantownButtons.swift
@@ -0,0 +1,18 @@
+//
+//  BeantownButtons.swift
+//  SwiftUI_Map_demo
+//
+//  Created by tom montgomery on 9/24/23.
+//
+
+import SwiftUI
+
+struct BeantownButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BeantownButtons()
+}
---
+++ SwiftUI_Map_demo/ItemInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemInfoView.swift
+//  SwiftUI_Map_demo
+//
+//  Created by tom montgomery on 9/27/23.
+//
+
+import SwiftUI
+
+struct ItemInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemInfoView()
+}
